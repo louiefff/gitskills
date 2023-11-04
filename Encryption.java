e9448e9f2f243b01046654efef6423916a4a1d9f
ZCJ <481209870@qq.com>
2023-11-04 15:56:59 +0800

    third

--- Encryption.java
+++ Encryption.java
@@ -1,4 +1,5 @@
 package EncryptionAndDecryption;
+import java.util.Random;
 public class Encryption {
 
 void encryption(String code){//加密方法
@@ -48,5 +49,78 @@ chararray[n]=(char) (chararray[n]-(n+4));
 String Code=String.copyValueOf(chararray);
 System.out.println("解密后:"+Code);
 }
+
+void judgment(String code) {//判断密码强度
+char []chararray=code.toCharArray();
+int number=0;
+int mini=0;
+int max=0;
+while(true) {
+if(chararray.length<8) {//密码长度小于8个字符为弱强度
+System.out.println("该密码为弱密码!!");break;	
+}
+else {
+for(int a=0;a<chararray.length;a++) {
+if(chararray[a]>='0' && chararray[a]<='9') {
+number++;	
+}
+
+if(chararray[a]>='a' && chararray[a]<='z') {
+mini++;	
+}
+
+if(chararray[a]>='A' && chararray[a]<='Z') {
+max++;	
+}
+}
+
+}
+if(number==chararray.length||mini==chararray.length||max==chararray.length) {//长度大于等于8个字符但只有一种字符类型为弱强度
+System.out.println("该密码为弱强度密码！！");break;	
+}
+
+if(mini==0||max==0) {//长度大于等于8包含至少一个数字和一个字母为中强度
+System.out.println("该密码为中强度密码！！");break;	
+}
+
+if(number!=0 && mini!=0 && max!=0) {//长度大于等于8并数字和大小写都有为高强度
+System.out.println("该密码为高强度密码！！");break;	
+}
+
+}
+}
+
+void passwordgeneration(int length) {
+char []codearray=new char[length];
+Random random = new Random();
+int number=0;
+int mini=0;
+int max=0;
+for(int i=0;i<length;i++) {
+int choice=random.nextInt(3)+1;
+
+if(i>=length/2&&max==0) {
+choice=1;	
+}
+if(i>=length/2&&mini==0) {
+choice=2;	
+}
+if(i>=length/2&&number==0) {
+choice=3;	
+}
+switch(choice) {
+case 1:codearray[i]=(char)(random.nextInt(26)+'A');max++;break;
+case 2:codearray[i]=(char)(random.nextInt(26)+'a');mini++;break;
+case 3:codearray[i]=(char)(random.nextInt(10)+'0');number++;break;
+}
+
+}
+StringBuilder codeString = new StringBuilder();
+for (int i = 0; i <codearray.length; i++) {
+    codeString.append(codearray[i]);
+}
+System.out.println("密码为"+codeString.toString());
+}
+
 
 }
--- Main.java
+++ Main.java
@@ -5,15 +5,19 @@ public class Main {
 	public static void main(String[] args) {
     Encryption e1=new Encryption();
     boolean i=true;
-    while(i) {
-    System.out.println("请输入密码:");
-    Scanner in =new Scanner(System.in);//输入密码
-    String code=in.next();
-    System.out.println("请输入选项:1.加密 2.解密 3.输入任意键退出");
+    while(i) {   
+    Scanner in =new Scanner(System.in);
+    String code;
+    System.out.println("==============================\r\n"
+    		+ "欢迎使用密码管理系统\r\n"
+    		+ "==============================\r\n"
+    		+ "请输入选项:\n1.加密 \n2.解密 \n3.判断密码强度 \n4.密码生成 \n5.输入任意键退出");
     int choice= in.nextInt();
     switch(choice) {
-    case 1: e1.encryption(code);break;
-    case 2: e1.decryption(code);break;
+    case 1:System.out.println("请输入密码:");  code=in.next();e1.encryption(code);break;
+    case 2:System.out.println("请输入密码:");  code=in.next();e1.decryption(code);break;
+    case 3:System.out.println("请输入密码:");  code=in.next();e1.judgment(code);break;
+    case 4:System.out.println("请输入密码长度:");int len=in.nextInt();e1.passwordgeneration(len); break;
     default:
     in.close();	i=false;
     }
